da3d2cbb3ab4005eb15e5b8e5e56f7f23c32d698
florianthiery <mail@fthiery.de>
2023-12-04 13:19:45 +0000

    Deploying to gh-pages from @ archaeolink-lod/samian@10a9e21e1f1a95206d5686d645ab2e00247c711b 🚀

---
+++ minivowl_result.js
@@ -0,0 +1,28 @@
+var minivowlresult={
+ "info": [
+  {
+   "description": "Created with pyowl2vowl (version 0.1) as part of the SPARQLing Unicorn QGIS Plugin"
+  }
+ ],
+ "nodes": [
+  {
+   "name": "Potform",
+   "type": "class",
+   "uri": "http://archaeology.link/ontology#Potform"
+  },
+  {
+   "name": "Activity",
+   "type": "class",
+   "uri": "http://www.w3.org/ns/prov#Activity"
+  }
+ ],
+ "links": [
+  {
+   "source": 0,
+   "target": 1,
+   "valueTo": "wasGeneratedBy",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/prov-o/wasGeneratedBy"
+  }
+ ]
+}
